882fb9df3f33f1b3b5c7ad23867e70b16addd267
Clément Baillet <c.bailletpro@icloud.com>
2025-10-08 17:25:29 +0200

    graphe data

---
+++ plot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+a = 1.0
+b = 1.0
+Nx = 50
+Ny = 50
+data = "data_jacobi.csv"
+
+Z = np.loadtxt(data, delimiter=",")
+print(Z)
+
+x = np.linspace(0,a,Nx+2)
+y = np.linspace(0,b,Ny+2)
+X,Y = np.meshgrid(x,y)
+
+
+# Tracé
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, cmap='viridis')
+
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("f(x,y)")
+
+plt.show()
